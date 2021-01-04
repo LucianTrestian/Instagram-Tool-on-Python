8a47440aba5cbe21904f65703a83a546800b741b
Lucian <luciantrestian007@gmail.com>
2021-01-04 14:56:43 +0200

    update code, continue to develope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from instabot import Bot
 from PIL import Image
-import glob
+import os
 import time
 import random
 
@@ -10,9 +10,10 @@ bot.login(username=input("Username="),
           password=input("Password="))
 
 
+
 def upload_image():
-    bot.upload_photo(image_list[int(input('Chosen image: ')) - 1],
-                     caption=input('Comment: '))
+    bot.upload_photo(image,
+                     caption=comment)
 
 
 while True:
@@ -25,23 +26,34 @@ while True:
         """)
 
     while command == 'upload':
-        print('From where you wanna post images ? (one the end write *.format, format for example jpg, png, gif)')
-        print('For example "F:/Photoshop/Wallpapers/*.jpg"')
-        path = input('Path = ')
-        image_list = []
-        for filename in glob.glob(path):
-            im = Image.open(filename)
-            image_list.append(im)
-        images = input('One images or more ? (one - for one images, more - for more)\n').lower()
-        if images == 'one':
+        images = input('Select mode: Solo, Multi)\n').lower()
+        if images == 'solo':
+            image = input(r'Select image, for example C:\images\image.png -> ')
+            comment = input('Comment -> ')
             upload_image()
             choice = input('Continue to upload images ? Yes / No\n').title()
             if choice == "Yes":
                 upload_image()
             elif choice == 'No':
                 break
-        elif images == 'more':
-            upload_image()
+        elif images == 'Multi':
+            print('For start choice folder(path)')
+            file_list = os.listdir(input('Path = '))
+            mod = input("Post all images (all) or only a certain number of pictures(certain)")
+            if mod == "all":
+                print(' WARNING, if you have many images, time for post all images can be long')
+                comment = input('You chose "all", now select comment for all images\n')
+                n = 0
+                while n != len(file_list):
+                    image = file_list[n]
+                    upload_image()
+                    n += 1
+            elif mod == 'certain':
+                chose_mod = input('You choice "certain" mod, you can chose it with position(for ex. 1 - 10) or named, chose mod\n')
+                if chose_mod == 'position':
+                    pos = input('Choice position, split it with space( ): ').split(' ')
+                    while (int(pos[0]) - 1) < 0 and int(pos[1]) - 1 > len(file_list):
+
 
     if command == "exit":
         exit()
